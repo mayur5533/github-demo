98de6879351aa2dc74004ef1989239a180bc76a6
mayur5533 <100755188+mayur5533@users.noreply.github.com>
2022-03-06 14:38:26 +0530

    initial commit on math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+add implementation
+def add(x,y):
+	return x+y
+subtract implementation
+def sub(x,y):
+	return x-y            #on master branch
+multiply implementation
+def mul(x,y):
+	return x*y           #implemented multiply fn on bug456
+divide implementation
+def div(x,y):
+	if y=0                # on master branch
+       return error_on
+    else
+        return x/y
+
